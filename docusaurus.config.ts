ea0c3504570097f4c1dfdb1b17a022da90a1e6c4
Daniel Ciochiu <daniel.ciochiu@gmail.com>
2026-03-26 09:20:29 +0200

    Updated project URL

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -16,10 +16,10 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://words.github.io',
+  url: 'https://krud-soft.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/words/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
